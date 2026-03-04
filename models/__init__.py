3336ffc83227ad8a20c3702f11e7adbe37aae5db
OrangeHu <132962750+orangehu19@users.noreply.github.com>
2026-03-04 11:39:57 +0800

    Refactor import statements in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
-# from models.ddm_new import *
-# from models.ddm import *
-# from models.ddm_mamba import *
-from models.ddm_high import *
+
+from models.ddm import *
 from models.restoration import *
+
